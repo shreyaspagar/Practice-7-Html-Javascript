37ac19e63520e11ff804c9fd6706e04c9a280027
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:19:44 +0530

    Add files via upload

---
+++ app19.js
@@ -0,0 +1,24 @@
+let quarter = 40;
+
+switch(quarter) {
+    case 1:
+        console.log("January,February,March");
+        break;
+        case 2:
+        console.log("April,May,June");
+        break;
+        case 3:
+        console.log("July,August,September");
+        break;
+        case 4:
+        console.log("October,November,December");
+        break;
+        default:
+        console.log("Not a Quarter!");
+}
+
+
+
+
+
+
---
+++ app7.js
@@ -0,0 +1,24 @@
+let marks = 20;
+
+if(marks >= 80) {
+    console.log("A+");
+}
+else if(marks >= 60) {
+    console.log("A");
+}
+else if(marks >= 33) {
+    console.log("B");
+}
+else if(marks < 33) {
+    console.log("F");
+}
+
+let month = "April";
+
+if(month === "January") {
+    console.log("Winter is here");
+}
+else if(month === "April") {
+    console.log("Summer is here");
+    
+}
